6d13ac5fee3f9dfa3a6e6e2b6b2a2d254ef1f6d0
watashi407 <71602488+watashi407@users.noreply.github.com>
2022-10-05 01:08:54 +0800

    try 1

--- components/experience/experience.tsx
+++ components/experience/experience.tsx
@@ -15,13 +15,14 @@ const experience = (props: Props) => {
         opacity: 1,
         x: 0,
       }}
-      className="h-screen flex flex-col relative text-lft max-w-full overflow-hidden text-center md:text-left md:flex-row px-15 justify-evenly mx-auto items-center"
+      className="h-screen flex flex-col relative text-lft overflow-hidden md:flex-row max-w-full px-15 justify-evenly mx-auto items-center"
     >
       <h3 className="absolute uppercase tracking-[15px] text-[#d9f99d] animate-pulse top-24 text-2xl">
         experience
       </h3>
 
-      <div className="w-full flex space-x-6 overflow-x-scroll p-15 snap-x snap-mandatory ">
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory ">
+        <ExperiencedCard />
         <ExperiencedCard />
         <ExperiencedCard />
       </div>
